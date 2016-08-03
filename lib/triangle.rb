92ecb9a9b393e6d5196a749d8881b0762d62bfc3
Mahati Gollamudi <github email address>
2016-08-02 22:03:14 -0400

    Triangle Laws Lab complete

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,32 @@
 class Triangle
-  # write code here
+
+  attr_reader :sideA, :sideB, :sideC
+
+  def initialize(sideA, sideB, sideC)
+    @sideA = sideA
+    @sideB = sideB
+    @sideC = sideC
+  end
+
+  def kind
+    if sideA <= 0 || sideB <= 0 || sideC <= 0
+      raise TriangleError
+    elsif ((sideA + sideB) <= sideC) || ((sideA + sideC) <= sideB) || ((sideB + sideC) <= sideA)
+      raise TriangleError
+    elsif sideA == sideB && sideB == sideC
+      return :equilateral
+    elsif sideA == sideB || sideA == sideC || sideB == sideC
+      return :isosceles
+    elsif sideA != sideB || sideA != sideC || sideB != sideC
+      return :scalene
+    else
+      raise TriangleErrror
+    end
+  end
+
+
+end
+
+class TriangleError < StandardError
+
 end
